8b1eb7e61edf822823dac8ddda91972be5d61e46
Khuất Tuấn Anhh <khuattuananh202002@gmail.com>
2021-12-30 21:59:54 +0700

    Update

---
+++ Web29-1/Controllers/Hello.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web29_1.Controllers
+{
+    public class Hello : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
