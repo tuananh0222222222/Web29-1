c263ddb8eed4e4e70068019de2f4d457fac2bbb9
Khuất Tuấn Anhh <khuattuananh202002@gmail.com>
2022-01-01 12:09:29 +0700

    all

---
+++ eShopsoluyion.Data/EF/EshopDBContext.cs
@@ -0,0 +1,26 @@
+﻿using eShopsoluyion.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopsoluyion.Data.EF
+{
+    public class EshopDBContext : DbContext
+    {
+        public EshopDBContext(DbContextOptions options) : base(options)
+        {
+        }
+         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> languages { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
---
+++ eShopsoluyion.Data/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopsoluyion.Data.Entities
+{
+    public class Category
+    {
+       public int Id { get; set; }
+       public int SortOder     { get; set; }
+       public int IsShowOnHome { get; set; }
+       public int ParentId     { get; set; }
+       public int Status { get; set; }
+    }
+}
